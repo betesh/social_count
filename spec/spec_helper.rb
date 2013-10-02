487b99b021d93e3158da8d3147cf216e6c37ef34
Isaac Betesh <ibetesh@springbig.com>
2013-10-02 14:51:20 -0400

    added test credentials for FB and Twitter API's

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'social_count'
 
+Dir[File.expand_path("#{__FILE__}/../support/*.rb")].each {|f| require f }
+
 RSpec.configure do |config|
   config.order = "random"
 end
---
+++ spec/support/test_credentials.rb
@@ -0,0 +1,12 @@
+class TestCredentials
+  attr_reader :twitter_consumer_key, :twitter_consumer_secret, :twitter_oauth_token, :twitter_oauth_token_secret, :fb_app_id, :fb_app_secret
+  def initialize twitter_consumer_key, twitter_consumer_secret, twitter_oauth_token, twitter_oauth_token_secret, fb_app_id, fb_app_secret
+    @twitter_consumer_key, @twitter_consumer_secret, @twitter_oauth_token, @twitter_oauth_token_secret, @fb_app_id, @fb_app_secret = twitter_consumer_key, twitter_consumer_secret, twitter_oauth_token, twitter_oauth_token_secret, fb_app_id, fb_app_secret
+  end
+  credentials_file = File.expand_path("#{__FILE__}/../credentials.rb")
+  unless File.exists?(credentials_file)
+    puts "Please configure your Facebook and Twitter application credentials by copying #{credentials_file}.example to #{credentials_file} and configuring the required values there appropriately."
+    puts "Then comment out the 'exit' line in #{File.expand_path(__FILE__)} before running rspec."
+    exit
+  end
+end
